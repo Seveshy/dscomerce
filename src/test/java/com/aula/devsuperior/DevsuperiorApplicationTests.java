452f7da4ca4d6fa230cfc270c8398528b4d16641
Daniel Major <daniel.alves@skytef.com.br>
2023-06-20 21:04:59 -0300

    Entidade User, banco H2

---
+++ src/test/java/com/aula/devsuperior/DevsuperiorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aula.devsuperior;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevsuperiorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
