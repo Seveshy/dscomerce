fde998874b618c1c39a5ca9608f046e8bc38ed35
Daniel Major <daniel.alves@skytef.com.br>
2023-07-16 11:58:00 -0300

    Tratando exceções com respostas customizadas

---
+++ src/main/java/com/aula/devsuperior/controller/handlers/ControllerExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.aula.devsuperior.controller.handlers;
+
+import java.time.Instant;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.aula.devsuperior.dto.CustomError;
+import com.aula.devsuperior.execptions.ResourceNotfoundException;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class ControllerExceptionHandler {
+    
+    @ExceptionHandler(ResourceNotfoundException.class)
+    public ResponseEntity<CustomError> resourceNotFound(ResourceNotfoundException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        CustomError err = new CustomError(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+}
