4856abbd0f57e6117c6c5d8753ec36c05cb755ed
Daniel Major <daniel.alves@skytef.com.br>
2023-07-25 16:12:33 -0300

    Customizando mensagem de validação

--- src/main/java/com/aula/devsuperior/controller/handlers/ControllerExceptionHandler.java
+++ src/main/java/com/aula/devsuperior/controller/handlers/ControllerExceptionHandler.java
@@ -4,10 +4,13 @@ import java.time.Instant;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import com.aula.devsuperior.dto.CustomError;
+import com.aula.devsuperior.dto.ValidationError;
 import com.aula.devsuperior.execptions.DatabaseException;
 import com.aula.devsuperior.execptions.ResourceNotfoundException;
 
@@ -23,10 +26,15 @@ public class ControllerExceptionHandler {
         return ResponseEntity.status(status).body(err);
     }
      
-    @ExceptionHandler(DatabaseException.class)
-    public ResponseEntity<CustomError> database(DatabaseException e, HttpServletRequest request) {
-        HttpStatus status = HttpStatus.BAD_REQUEST;
-        CustomError err = new CustomError(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<CustomError> methodArgumentNotValidation(MethodArgumentNotValidException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
+        ValidationError err = new ValidationError(Instant.now(), status.value(), "Dados invalidos", request.getRequestURI());
+
+        for(FieldError f : e.getBindingResult().getFieldErrors()) {
+            err.addError(f.getField(), f.getDefaultMessage());
+        }
+
         return ResponseEntity.status(status).body(err);
     }
 }
