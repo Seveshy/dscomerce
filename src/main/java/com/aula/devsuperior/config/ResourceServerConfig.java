3433a6afc080fa1d4942d573d7558791a7d74623
Daniel Major <daniel.alves@skytef.com.br>
2023-09-13 13:07:08 -0300

    Config de cors

--- src/main/java/com/aula/devsuperior/config/ResourceServerConfig.java
+++ src/main/java/com/aula/devsuperior/config/ResourceServerConfig.java
@@ -8,6 +8,8 @@ import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 @Configuration
 @EnableResourceServer
@@ -15,6 +17,16 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
     @Autowired
     private Environment env;
+
+    @Autowired
+    private JwtTokenStore tokenStore;
+
+    private static final String[] PUBLIC = { "/oauth/token", "/h2-console/**" };
+
+    @Override
+    public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+        resources.tokenStore(tokenStore);
+    }
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
